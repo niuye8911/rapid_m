c92d73e817b7ac298e3415b9c7cc07f88955d715
niuye8911 <niuye8911@gmail.com>
2019-04-24 20:52:42 -0400

    apps/ models/ buckets loader

--- BucketSelector.py
+++ BucketSelector.py
@@ -9,6 +9,8 @@ from Classes.App import *
 from Classes.PModel import *
 from Classes.SlowDownProfile import *
 from Classes.AppSysProfile import *
+from Classes.Bucket import *
+from Utility import *
 import pandas as pd
 import json
 
@@ -16,13 +18,32 @@ import json
 def bucketSelect(active_apps_file, SELECTOR="P_M"):
     with open(active_apps_file, 'r') as file:
         active_apps = json.load(file)
+
         if SELECTOR == "P_M":
             return pmSelect(active_apps)
 
 
 def pmSelect(active_apps):
+    # get all the apps
     apps = getActiveApps(active_apps)
+    # get all the P_models {app_name: {bucket_name: model }}
+    models = loadAppModels(apps)
+    # convert apps to buckets
+    buckets = genBuckets(apps, models)
 
+
+def genBuckets(apps, models):
+    buckets = {}
+    for app in apps:
+        dir = app['dir']
+        app = app['app']
+        buckets[app.name]={}
+        for bucket_name, info in app.cluster_info.items():
+            bucket = Bucket(info['cluster'], models[app.name][bucket_name],
+                            dir + '/cost.csv', dir + '/mv.csv', info['env'])
+            buckets[app.name][bucket_name] = bucket
+        RAPID_info("Bucket Loader", "loaded Buckets for " + app.name)
+    return buckets
 
 def getActiveApps(active_apps):
     '''return a list of App instances from the profile'''
@@ -34,5 +55,20 @@ def getActiveApps(active_apps):
             # inactive apps
             continue
         application = App(app['dir'] + '/profile.json')
-        apps.append({'app': application, 'budget': app['budget']})
+        apps.append({
+            'app': application,
+            'budget': app['budget'],
+            'dir': app['dir']
+        })
     return apps
+
+
+def loadAppModels(apps):
+    models = {}
+    for app in apps:
+        app = app['app']
+        models[app.name] = {}
+        for bucket_name, info in app.model_params.items():
+            models[app.name][bucket_name] = PModel(info)
+        RAPID_info("Model Loader", "loaded P-Models for " + app.name)
+    return models
