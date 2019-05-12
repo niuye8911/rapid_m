dfa2eee88fc39f9311459d9935d960af7ec7f1d9
niuye8911 <niuye8911@gmail.com>
2019-05-12 15:00:59 -0400

    new batch of data

---
+++ testData/appself/PModelCombine.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+BASE_DIR = '/home/liuliu/Research/rapid_m_backend_server/testData/appself/'
+APPS = ['ferret','swaptions','bodytrack','facedetect']
+
+HEADER_DONE = False
+
+for app in APPS:
+    p_small = pd.read_csv(BASE_DIR+app+"-perf-small.csv")
+    p_big = pd.read_csv(BASE_DIR+app+"-perf-big.csv")
+    p_combined = pd.concat([p_small,p_big])
+    p_combined.to_csv(app+"-perf-combined.csv",index=False)
