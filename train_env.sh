8c2c0d2bad7da220d3f0652fa2be0c727ea3278e
niuye8911 <niuye8911@gmail.com>
2019-05-12 15:00:39 -0400

    demo tools

---
+++ train_env.sh
@@ -0,0 +1,5 @@
+git checkout examples/example_machine_empty.json
+
+rm -r outputs/machine 2> /dev/null
+
+python3 RapidMain.py --flow TRAIN_ENV --path2machine ./examples/example_machine_empty.json --envdata ./mmodelfile.csv --dir outputs/machine
