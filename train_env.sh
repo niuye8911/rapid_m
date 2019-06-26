ea730b20351d7b231a5c9a72a6300ba2151679ed
liuliu <niuye8911@gmail.com>
2019-06-26 11:29:36 -0400

    testting OPTION

--- train_env.sh
+++ train_env.sh
@@ -2,4 +2,4 @@ git checkout examples/example_machine_empty.json
 
 rm -r outputs/machine 2> /dev/null
 
-python3 RapidMain.py --flow TRAIN_ENV --path2machine ./examples/example_machine_empty.json --envdata ./testData/mmodelfile.csv --dir outputs/machine -d
+python3 RapidMain.py --flow TRAIN_ENV --path2machine ./examples/example_machine_empty.json --envdata ./testData/mmodelfile.csv --dir outputs/machine -d --test
