8e6d3af52fb291e084e3eb399205eaeabfcc8524
liuliu <niuye8911@gmail.com>
2019-07-12 10:12:15 -0400

    add nn and svm cmd

--- init_all.sh
+++ init_all.sh
@@ -5,3 +5,8 @@ python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --ap
 python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/ferret-sys.csv --appdata testData/halfandhalf/ferret-perf.csv --dir outputs/ferret --test --app ferret
 
 python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/facedetect-sys.csv --appdata testData/halfandhalf/facedetect-perf.csv --dir outputs/facedetect --test --app facedetect
+
+python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/nn-sys.csv --appdata testData/halfandhalf/nn-perf.csv --dir outputs/nn --test --app nn
+
+python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/svm-sys.csv --appdata testData/halfandhalf/svm-perf.csv --dir outputs/svm --test --app svm
+
