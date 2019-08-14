875b9863109bda70c87041c9908e82125a6e9abb
liuliu <niuye8911@gmail.com>
2019-08-13 20:07:50 -0400

    test driver

---
+++ TestScript/Rapid_M_Thread.py
@@ -0,0 +1,45 @@
+''' a thread with subprocess callback'''
+import threading
+import time
+import os
+
+
+class Rapid_M_Thread(threading.Thread):
+    def __init__(self, callback=None, callback_args=None, *args, **kwargs):
+        target = kwargs.pop('target')
+        dir = kwargs.pop('dir')
+        cmd = kwargs.pop('cmd')
+        app_time = kwargs.pop('app_time')
+        app = kwargs.pop('app')
+        super(Rapid_M_Thread, self).__init__(target=self.target_with_callback,
+                                             args=(app_time, dir, cmd, app))
+        self.callback = callback
+        self.method = target
+        self.callback_args = callback_args
+
+    def target_with_callback(self, app_time, dir, cmd, app):
+        self.method(dir, app_time, cmd, app)
+        if self.callback is not None:
+            print(*self.callback_args)
+            self.callback(*self.callback_args)
+
+
+def rapid_worker(dir, app_time, cmd, app):
+    # run command under dir and record time
+    os.chdir(dir)
+    print("executing:", " ".join(cmd))
+    start_time = time.time()
+    os.system(" ".join(cmd))
+    exec_time = time.time() - start_time
+    app_time[app] = exec_time
+
+
+def rapid_callback(app, cmd, app_time):
+    print(app, '*****************finished')
+    # this is run after your thread end
+    while -1 in app_time.values():
+        os.chdir(
+            '/home/liuliu/Research/rapid_m_backend_server/TestScript/tmp/')
+        print(app, '****************waiting')
+        os.system(" ".join(cmd))
+    return
--- TestScript/TestDriver.py
+++ TestScript/TestDriver.py
@@ -1,38 +1,127 @@
 ''' Test driver to run multiple apps together '''
-import os, sys, imp, subprocess
+import os, sys, imp, subprocess, json
+from itertools import combinations
+from concurrent.futures import ThreadPoolExecutor as Pool
+
 RAPIDS_DIR = '/home/liuliu/Research/rapidlib-linux/modelConstr/'
 RAPIDS_SOURCE_DIR = '/home/liuliu/Research/rapidlib-linux/modelConstr/Rapids/'
+RAPIDS_M_DIR = '/home/liuliu/Research/rapid_m_backend_server/'
 sys.path.append(os.path.dirname(RAPIDS_DIR))
+sys.path.append(os.path.dirname(RAPIDS_M_DIR))
 sys.path.append(os.path.dirname(RAPIDS_SOURCE_DIR))
 
 import Rapids.Classes
 import Rapids.App.AppSummary
-
-apps = ['swaptions', 'ferret', 'bodytrack', 'svm', 'nn', 'facedetect']
+from BucketSelector import bucketSelect
+from Rapid_M_Thread import Rapid_M_Thread, rapid_callback, rapid_worker
+#apps = ['swaptions', 'ferret', 'bodytrack', 'svm', 'nn', 'facedetect']
+apps = ['swaptions', 'bodytrack']
 APP_MET_PREFIX = '/home/liuliu/Research/rapidlib-linux/modelConstr/appExt/'
+TEST_APP_FILE = './test_app_file.txt'
+
+app_info = {}
+
 
 # preparation
-app_info = {}
-for app in apps:
-    # init all appmethods
-    file_loc = APP_MET_PREFIX + app + "Met.py"
-    module = imp.load_source("", file_loc)
-    appMethod = module.appMethods("", app)
-    # prepare run_dir for all apps
-    run_dir = os.getcwd()+"/run/" + app
-    if not os.path.exists(run_dir):
-        os.makedirs(run_dir)
-    app_info[app] = {'met': appMethod, 'dir': run_dir}
+def genInfo():
+    for app in apps:
+        # init all appmethods
+        file_loc = APP_MET_PREFIX + app + "Met.py"
+        module = imp.load_source("", file_loc)
+        appMethod = module.appMethods("", app)
+        # prepare run_dir for all apps
+        run_dir = os.getcwd() + "/run/" + app
+        if not os.path.exists(run_dir):
+            os.makedirs(run_dir)
+        app_info[app] = {'met': appMethod, 'dir': run_dir}
+
 
 # gen apps' gt
-for app,info in app_info.items():
-    if app is not "bodytrack":
-        pass
-    cmd = info['met'].getCommand(qosRun=True)
-    working_dir = info['dir']
-    # change working dir
-    os.chdir(working_dir)
-    # run all command and get the output
-    stresser = subprocess.Popen(" ".join(cmd),
-                                shell=True,
-                                preexec_fn=os.setsid)
+def genGT():
+    for app, info in app_info.items():
+        if app is not "facedetect":
+            pass
+        cmd = info['met'].getCommand(qosRun=True)
+        working_dir = info['dir']
+        # change working dir
+        os.chdir(working_dir)
+        # run all command and get the output
+        stresser = subprocess.Popen(" ".join(cmd),
+                                    shell=True,
+                                    preexec_fn=os.setsid)
+
+
+# generate the app running instances
+def genRunComb():
+    num_of_app = len(apps)
+    combs = {}
+    total = 0
+    for i in range(2, num_of_app + 1):
+        combs[i] = list(combinations(apps, i))
+        total += len(combs[i])
+    print('total num:', total)
+    return combs
+
+
+def run_a_comb(apps):
+    '''comb is a list of app names'''
+    progress_map = {}
+    thread_list = []
+    configs = bucketSelect(TEST_APP_FILE, SELECTOR="INDIVIDUAL")
+    print(configs[1], configs[2])
+    exit(1)
+    for app in apps:
+        progress_map[app] = -1
+        appMethod = app_info[app]['met']
+        run_dir = app_info[app]['dir']
+        max_budget = appMethod.max_cost * appMethod.fullrun_units
+
+        app_cmd = appMethod.getCommand(qosRun=True)
+        app_thread = Rapid_M_Thread(name=app + "_thread",
+                                    target=rapid_worker,
+                                    dir=run_dir,
+                                    cmd=app_cmd,
+                                    app_time=progress_map,
+                                    app=app,
+                                    callback=rapid_callback,
+                                    callback_args=(app, app_cmd, progress_map))
+        thread_list.append(app_thread)
+    # kick off all threads
+    for t in thread_list:
+        t.start()
+    print("all workers started")
+    for t in thread_list:
+        t.join()
+    print(progress_map)
+
+
+def run(combs, mode="INDIVIDUAL"):
+    qos = {}
+    exec_time = {}
+    if mode == "INDIVIDUAL":
+        for num_of_app, comb in combs.items():
+            for apps in comb:
+                update_app_file(apps)
+                run_a_comb(apps)
+                exit(1)
+
+
+def update_app_file(apps):
+    active_apps = {}
+    with open(TEST_APP_FILE, 'r') as file:
+        active_apps = json.load(file)
+        applications = active_apps['applications']
+        for app in applications:
+            app_name = app['id']
+            if app_name not in apps:
+                app['status'] = 0
+            else:
+                app['status'] = 1
+                app['budget'] = app_info[app_name]['met'].max_cost
+    with open(TEST_APP_FILE, 'w') as file:
+        json.dump(active_apps, file, indent=2)
+
+
+genInfo()
+combs = genRunComb()
+run(combs)
