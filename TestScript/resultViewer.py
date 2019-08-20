a90303e00628e41051e86aa03288c7156e517f10
liuliu <niuye8911@gmail.com>
2019-08-20 09:37:10 -0400

    resultview

--- TestScript/resultViewer.py
+++ TestScript/resultViewer.py
@@ -6,14 +6,14 @@ import json
 from collections import OrderedDict
 # read in all files
 
-modes = ['P','P_M','INDIVIDUAL','N']
-budgets = [0.8,1.0,1.5]
-apps = ['swaptions','ferret','bodytrack','svm','nn','facedetect']
+modes = ['P', 'P_M', 'INDIVIDUAL', 'N']
+budgets = [0.8, 1.0, 1.5]
+apps = ['swaptions', 'ferret', 'bodytrack', 'svm', 'nn', 'facedetect']
 
 mvs = OrderedDict()
 exceeds = OrderedDict()
 misses = OrderedDict()
-datas = {'mv':mvs,'exceed':exceeds,'miss_pred':misses}
+datas = {'mv': mvs, 'exceed': exceeds, 'miss_pred': misses}
 
 n_groups = 5
 
@@ -24,48 +24,53 @@ def readFile(dir):
         exceeds[budget] = {}
         misses[budget] = {}
         # read in all files
-        for data,result in datas.items():
+        for data, result in datas.items():
             for mode in modes:
                 result[budget][mode] = []
-                file_name = dir+'/'+data+'_'+mode+'_'+str(budget)+'.json'
+                file_name = dir + '/' + data + '_' + mode + '_' + str(
+                    budget) + '.json'
                 with open(file_name) as json_file:
-                    f = json.load(json_file,object_pairs_hook=OrderedDict)
-                    for num,values in f.items():
+                    f = json.load(json_file, object_pairs_hook=OrderedDict)
+                    for num, values in f.items():
                         c = list(values.values())
-                        result[budget][mode].append(sum(c)/len(c))
+                        result[budget][mode].append(sum(c) / len(c))
+
 
 def draw():
-    sub_graphs = ['exceed','miss_pred','mv']
+    sub_graphs = ['exceed', 'miss_pred', 'mv']
     for budget in budgets:
         id = 0
-        fig,axes = plt.subplots(nrows=3)
+        fig, axes = plt.subplots(nrows=3)
         fig.tight_layout()
-        bar_width=0.2
+        bar_width = 0.2
         opacity = 0.8
         index = np.arange(n_groups)
         plt.xlabel('Num Of Apps')
-        plt.xticks(index+bar_width,range(2,n_groups+2))
+        plt.xticks(index + bar_width, range(2, n_groups + 2))
         rects = []
         for data in sub_graphs:
-            result=datas[data]
+            result = datas[data]
             # assign bars
             i = 0
-            axes[id].set_ylim([0.0,1.0])
-            axes[id].set_yticks([0.0,0.25,0.5,0.75,1.0])
-            rects_done = len(rects)>0
+            axes[id].set_ylim([0.0, 1.0])
+            axes[id].set_yticks([0.0, 0.25, 0.5, 0.75, 1.0])
+            rects_done = len(rects) > 0
             for mode in modes:
-                rect = axes[id].bar(index+i*bar_width, result[budget][mode], bar_width, alpha=opacity)
+                rect = axes[id].bar(index + i * bar_width,
+                                    result[budget][mode],
+                                    bar_width,
+                                    alpha=opacity)
                 if not rects_done:
                     rects.append(rect)
-                i+=1
+                i += 1
             axes[id].set(ylabel=data)
-            axes[id].yaxis.grid(which="major",linestyle='--', linewidth=0.7)
+            axes[id].yaxis.grid(which="major", linestyle='--', linewidth=0.7)
             id += 1
         #axes[0].axis("off")
-        fig.legend(rects,modes,loc='upper center',ncol=4)
+        fig.legend(rects, modes, loc='upper center', ncol=4)
         plt.subplots_adjust(bottom=0.1, left=0.1, top=0.9)
-        plt.savefig('result_'+str(budget)+'.png')
+        plt.savefig('result_' + str(budget) + '.png')
 
 
-readFile('./aug18')
+readFile('./aug19')
 draw()
