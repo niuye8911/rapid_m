d1277de994f661d1485ed28cac544be5f8f31ffe
liuliu <niuye8911@gmail.com>
2019-10-18 10:31:02 -0400

    add x264

--- update_profile.sh
+++ update_profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # copy the cost and mv profile from rapidS
 
-apps="facedetect swaptions bodytrack ferret svm nn"
+apps="facedetect swaptions bodytrack ferret svm nn x264"
 
 for app in $apps
 do
@@ -9,4 +9,5 @@ do
  cp ~/Research/rapidlib-linux/modelConstr/Rapids/outputs/$app/$app-mv.fact /var/www/html/rapid_server/storage/apps/algaesim-$app/mv.csv
  cp ~/Research/rapidlib-linux/modelConstr/Rapids/outputs/$app/$app-cost.fact ~/Research/rapid_m_backend_server/outputs/$app/cost.csv
  cp ~/Research/rapidlib-linux/modelConstr/Rapids/outputs/$app/$app-cost.fact /var/www/html/rapid_server/storage/apps/algaesim-$app/cost.csv
+ cp ~/Research/rapidlib-linux/modelConstr/Rapids/outputs/$app/$app-*.csv ~/Research/rapid_m_backend_server/testData/halfandhalf
 done
