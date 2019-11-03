1fcff09574f73048dc2c2352f4935a09246ec6a2
liuliu <niuye8911@gmail.com>
2019-11-03 14:59:08 -0500

    add timer

--- init_all.sh
+++ init_all.sh
@@ -1,13 +1,13 @@
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/swaptions-sys.csv --appdata testData/halfandhalf/swaptions-perf.csv --dir outputs/swaptions --test --app swaptions
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/swaptions-sys.csv --appdata testData/halfandhalf/swaptions-perf.csv --dir outputs/swaptions --test --app swaptions
 
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/bodytrack-sys.csv --appdata testData/halfandhalf/bodytrack-perf.csv --dir outputs/bodytrack --test --app bodytrack
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/bodytrack-sys.csv --appdata testData/halfandhalf/bodytrack-perf.csv --dir outputs/bodytrack --test --app bodytrack
 
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/ferret-sys.csv --appdata testData/halfandhalf/ferret-perf.csv --dir outputs/ferret --test --app ferret
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/ferret-sys.csv --appdata testData/halfandhalf/ferret-perf.csv --dir outputs/ferret --test --app ferret
 
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/facedetect-sys.csv --appdata testData/halfandhalf/facedetect-perf.csv --dir outputs/facedetect --test --app facedetect
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/facedetect-sys.csv --appdata testData/halfandhalf/facedetect-perf.csv --dir outputs/facedetect --test --app facedetect
 
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/nn-sys.csv --appdata testData/halfandhalf/nn-perf.csv --dir outputs/nn --test --app nn
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/nn-sys.csv --appdata testData/halfandhalf/nn-perf.csv --dir outputs/nn --test --app nn
 
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/svm-sys.csv --appdata testData/halfandhalf/svm-perf.csv --dir outputs/svm --test --app svm
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/svm-sys.csv --appdata testData/halfandhalf/svm-perf.csv --dir outputs/svm --test --app svm
 
-python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/x264-sys.csv --appdata testData/halfandhalf/x264-perf.csv --dir outputs/x264 --test --app x264
+time python3 RapidMain.py --flow INIT --path2app examples/example_app_empty.json --apppfs testData/halfandhalf/x264-sys.csv --appdata testData/halfandhalf/x264-perf.csv --dir outputs/x264 --test --app x264
